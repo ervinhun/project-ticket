8974f57614e0f4e76e37fded024849b9456da6ce
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-03-20 13:50:44 +0100

    Proba3

--- src/main/java/dk/easv/ticket/Controllers/NewEventController.java
+++ src/main/java/dk/easv/ticket/Controllers/NewEventController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class NewEventController implements Initializable {
+
     @FXML private DatePicker dateEnd;
     @FXML private Spinner spEndHour;
     @FXML private Spinner spEndMinute;
